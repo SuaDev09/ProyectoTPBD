f45038bb50269ce6e64c1364d54e217d7939adf6
SuaDev09 <francisco.suarez.bastidas@uabc.edu.mx>
2022-05-23 15:39:54 -0700

    hola

---
+++ ProyectoTPBD/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoTPBD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hola";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "hola mundo hola mundo.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Jiijijia.";
+
+            return View();
+        }
+    }
+}
